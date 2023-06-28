938b837b90fcdbec6c678202c22439f6d39816b3
Ethan Rickert <ethanrickert0429@gmail.com>
2023-06-28 06:52:53 -0400

    changed icons

--- background.js
+++ background.js
@@ -11,21 +11,19 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 });
 
 async function embedVideo() {
-  const destinyChannelId = "UC554eY5jNUfDq3yDOJYirOQ";
   try {
     const embedElement = document.getElementById("embed");
     if (embedElement) {
       const iframe = document.createElement("iframe");
       iframe.style.width = "100%";
       iframe.style.height = "100%";
-      iframe.src = `https://www.youtube.com/embed/live_stream?channel=${destinyChannelId}`;
+      iframe.src = `https://www.youtube.com/embed/live_stream?channel=UCSJ4gkVC6NrvII8umztf0Ow`; // Embed version of the video
       iframe.style.position = "relative"; // Required for z-index to work
       iframe.style.zIndex = "9999"; // Bring to the forefront
       iframe.frameBorder = "0";
       embedElement.appendChild(iframe);
     }
   } catch (error) {
-    console.log("Stream is likely offline...");
     console.error("Error:", error);
   }
 }
