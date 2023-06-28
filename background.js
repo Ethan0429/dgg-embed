7376c21ba033a7b3d6499c50359b0d8be14c4233
Ethan Rickert <ethanrickert0429@gmail.com>
2023-06-28 02:32:08 -0400

    works now

--- background.js
+++ background.js
@@ -10,31 +10,22 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   }
 });
 
-function embedVideo() {
-  fetch("https://youtube.com/channel/@destiny/live")
-    .then((response) => response.text())
-    .then((html) => {
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(html, "text/html");
-      const linkCanonical = doc.querySelector('link[rel="canonical"]');
-      if (linkCanonical) {
-        const href = linkCanonical.getAttribute("href");
-        const videoId = href.split("watch?v=")[1]; // Get video ID from href
-        const embedElement = document.getElementById("embed");
-        if (embedElement) {
-          const iframe = document.createElement("iframe");
-          iframe.style.width = "100%";
-          iframe.style.height = "100%";
-          iframe.src = `https://www.youtube.com/embed/${videoId}`; // Embed version of the video
-          iframe.style.position = "relative"; // Required for z-index to work
-          iframe.style.zIndex = "9999"; // Bring to the forefront
-          iframe.frameborder = "0";
-          embedElement.appendChild(iframe);
-        }
-      }
-    })
-    .catch((error) => {
-      console.log("Stream likely offline...");
-      console.error("Error:", error);
-    });
+async function embedVideo() {
+  const destinyChannelId = "UC554eY5jNUfDq3yDOJYirOQ";
+  try {
+    const embedElement = document.getElementById("embed");
+    if (embedElement) {
+      const iframe = document.createElement("iframe");
+      iframe.style.width = "100%";
+      iframe.style.height = "100%";
+      iframe.src = `https://www.youtube.com/embed/live_stream?channel=${destinyChannelId}`;
+      iframe.style.position = "relative"; // Required for z-index to work
+      iframe.style.zIndex = "9999"; // Bring to the forefront
+      iframe.frameBorder = "0";
+      embedElement.appendChild(iframe);
+    }
+  } catch (error) {
+    console.log("Stream is likely offline...");
+    console.error("Error:", error);
+  }
 }
