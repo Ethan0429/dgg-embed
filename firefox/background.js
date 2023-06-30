ecb4f97dacf80696bf42a0f23927a0e0e65f0611
Ethan Rickert <ethanrickert0429@gmail.com>
2023-06-30 01:45:44 -0400

    added firefox addon version

---
+++ firefox/background.js
@@ -0,0 +1,84 @@
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (
+    changeInfo.status === "complete" &&
+    tab.url === "https://www.destiny.gg/bigscreen"
+  ) {
+    browser.tabs.executeScript(tabId, {
+      code: "(" + embedVideo.toString() + ")();",
+    });
+  }
+});
+
+async function embedVideo() {
+  try {
+    const destinyChannelId = "UC554eY5jNUfDq3yDOJYirOQ";
+    let embedElement = document.getElementById("embed");
+
+    if (embedElement) {
+      console.log("Removing existing embed element");
+      embedElement.remove();
+    }
+
+    embedElement = document.createElement("div");
+    embedElement.id = "dgg-embed";
+    embedElement.style.width = "100%";
+    embedElement.style.height = "auto";
+    embedElement.style.aspectRatio = "16 / 9";
+
+    const iframe = document.createElement("iframe");
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+    iframe.src = `https://www.youtube.com/embed/live_stream?channel=${destinyChannelId}`;
+    iframe.style.position = "relative"; // Required for z-index to work
+    iframe.style.zIndex = "9999"; // Bring to the forefront
+    iframe.frameBorder = "0";
+
+    embedElement.appendChild(iframe);
+
+    const streamWrapElement = document.getElementById("stream-wrap");
+    if (streamWrapElement) {
+      streamWrapElement.appendChild(embedElement);
+    } else {
+      console.error("stream-wrap element not found");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+async function embedVideo() {
+  try {
+    const destinyChannelId = "UC554eY5jNUfDq3yDOJYirOQ";
+    let embedElement = document.getElementById("embed");
+
+    if (embedElement) {
+      console.log("Removing existing embed element");
+      embedElement.remove();
+    }
+
+    embedElement = document.createElement("div");
+    embedElement.id = "dgg-embed";
+    embedElement.style.width = "100%";
+    embedElement.style.height = "auto";
+    embedElement.style.aspectRatio = "16 / 9";
+
+    const iframe = document.createElement("iframe");
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+    iframe.src = `https://www.youtube.com/embed/live_stream?channel=${destinyChannelId}`;
+    iframe.style.position = "relative"; // Required for z-index to work
+    iframe.style.zIndex = "9999"; // Bring to the forefront
+    iframe.frameBorder = "0";
+
+    embedElement.appendChild(iframe);
+
+    const streamWrapElement = document.getElementById("stream-wrap");
+    if (streamWrapElement) {
+      streamWrapElement.appendChild(embedElement);
+    } else {
+      console.error("stream-wrap element not found");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
