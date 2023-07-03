94885b4a2e8a2ebe577ccae88f36754364f80c39
Ethan Rickert <ethanrickert0429@gmail.com>
2023-07-03 19:53:35 -0400

    implemented kick/youtube embed functionality

--- background.js
+++ background.js
@@ -1,48 +1,64 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (
     changeInfo.status === "complete" &&
-    tab.url === "https://www.destiny.gg/bigscreen"
+    /^https:\/\/www.destiny.gg\/bigscreen/.test(tab.url)
   ) {
-    chrome.scripting.executeScript({
-      target: { tabId: tabId },
-      function: embedVideo,
+    chrome.storage.sync.get("platform", function (data) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        function: (platform) => {
+          // embedVideo function definition here
+          async function embedVideo() {
+            try {
+              const destinyChannelId = "UC554eY5jNUfDq3yDOJYirOQ";
+              const destinyKickId = "destiny"; // Replace with actual Twitch ID
+              let embedElement = document.getElementById("embed");
+
+              if (embedElement) {
+                console.log("Removing existing embed element");
+                embedElement.remove();
+              }
+
+              embedElement = document.createElement("div");
+              embedElement.id = "dgg-embed";
+              embedElement.style.width = "100%";
+              embedElement.style.height = "auto";
+              embedElement.style.aspectRatio = "16 / 9";
+
+              const iframe = document.createElement("iframe");
+              iframe.style.width = "100%";
+              iframe.style.height = "100%";
+              iframe.style.position = "relative"; // Required for z-index to work
+              iframe.style.zIndex = "9999"; // Bring to the forefront
+              iframe.frameBorder = "0";
+
+              // Set the source of the iframe based on the platform
+              if (platform === "youtube") {
+                iframe.src = `https://www.youtube.com/embed/live_stream?channel=${destinyChannelId}`;
+              } else if (platform === "kick") {
+                iframe.src = `https://player.kick.com/${destinyKickId}`;
+              } else {
+                console.error("Unknown platform: " + platform);
+                return;
+              }
+
+              embedElement.appendChild(iframe);
+
+              const streamWrapElement = document.getElementById("stream-wrap");
+              if (streamWrapElement) {
+                streamWrapElement.appendChild(embedElement);
+              } else {
+                console.error("stream-wrap element not found");
+              }
+            } catch (error) {
+              console.error("Error:", error);
+            }
+          }
+          // Call the function after defining it
+          embedVideo(platform);
+        },
+        args: [data.platform],
+      });
     });
   }
 });
-
-async function embedVideo() {
-  try {
-    const destinyChannelId = "UC554eY5jNUfDq3yDOJYirOQ";
-    let embedElement = document.getElementById("embed");
-
-    if (embedElement) {
-      console.log("Removing existing embed element");
-      embedElement.remove();
-    }
-
-    embedElement = document.createElement("div");
-    embedElement.id = "dgg-embed";
-    embedElement.style.width = "100%";
-    embedElement.style.height = "auto";
-    embedElement.style.aspectRatio = "16 / 9";
-
-    const iframe = document.createElement("iframe");
-    iframe.style.width = "100%";
-    iframe.style.height = "100%";
-    iframe.src = `https://www.youtube.com/embed/live_stream?channel=${destinyChannelId}`;
-    iframe.style.position = "relative"; // Required for z-index to work
-    iframe.style.zIndex = "9999"; // Bring to the forefront
-    iframe.frameBorder = "0";
-
-    embedElement.appendChild(iframe);
-
-    const streamWrapElement = document.getElementById("stream-wrap");
-    if (streamWrapElement) {
-      streamWrapElement.appendChild(embedElement);
-    } else {
-      console.error("stream-wrap element not found");
-    }
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
